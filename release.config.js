3d1e0ea1e8db6c57bf4dc9d3a0fd6fa03ea29a06
Joshua Lovins <josh@Joshuas-Air.ds.mclovins.org>
2024-05-18 16:28:53 -0400

    feat: adawda

--- release.config.js
+++ release.config.js
@@ -5,6 +5,17 @@ module.exports = {
     "branches": [
         "main"
       ],
+      "plugins": [
+        "@droidsolutions-oss/semantic-release-update-file",
+        "@droidsolutions-oss/semantic-release-nuget",
+        [
+          "@semantic-release/git",
+          {
+            "assets": ["package.json", "package-lock.json", "CHANGELOG.md", "Directory.Build.props"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+          }
+        ]
+      ],
       "nugetServer": "https://nuget.pkg.github.com/jdlovins/index.json",
       "projectPath": "apps/my-api/Project.Apps.MyApi.csproj",
       "files": [
