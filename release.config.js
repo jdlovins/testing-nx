ce183a816c3a2b8e02b2f3d69d7b550114e23634
Joshua Lovins <josh@Joshuas-Air.ds.mclovins.org>
2024-05-18 19:30:31 -0400

    feat: changed thing:

--- release.config.js
+++ release.config.js
@@ -2,32 +2,32 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-    "branches": [
-        "main"
-      ],
-      "plugins": [
-        "@droidsolutions-oss/semantic-release-update-file",
-        "@droidsolutions-oss/semantic-release-nuget",
-        [
-          "@semantic-release/git",
-          {
-            "assets": ["package.json", "package-lock.json", "Directory.Build.props"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-          }
-        ]
-      ],
-      "nugetServer": "https://nuget.pkg.github.com/jdlovins/index.json",
-      "projectPath": "apps/my-api/Project.Apps.MyApi.csproj",
-      "files": [
-        {
-          "path": ["apps/my-api/Directory.Build.props"], // configure update-file plugin to update fields in Directory.Build.props
-          "type": "xml",
-          "replacements": [
-            {
-              "key": "Version",
-              "value": "${nextRelease.version}"
-            }
-          ]
-        }
-      ]
-  };
+  "branches": [
+    "main",
+    {
+      name: 'beta',
+      prerelease: true
+    }
+  ],
+  "plugins": [
+    "@droidsolutions-oss/semantic-release-update-file",
+    "@droidsolutions-oss/semantic-release-nuget",
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json", "package-lock.json", "Directory.Build.props"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ],
+  "nugetServer": "https://nuget.pkg.github.com/jdlovins/index.json",
+  "projectPath": "apps/my-api/Project.Apps.MyApi.csproj",
+  "files": [{
+    "path": ["apps/my-api/Directory.Build.props"], // configure update-file plugin to update fields in Directory.Build.props
+    "type": "xml",
+    "replacements": [{
+      "key": "Version",
+      "value": "${nextRelease.version}"
+    }]
+  }]
+};
