3e904b21512f8a5882af12ac2e74fb57d071b8c4
Joshua Lovins <josh@Joshuas-Air.ds.mclovins.org>
2024-05-18 14:51:14 -0400

    a lot

--- release.config.js
+++ release.config.js
@@ -20,5 +20,21 @@ module.exports = {
         ]
       ],
       "nugetServer": "https://nuget.pkg.github.com/jdlovins/index.json",
-      "projectPath": "apps/my-api/Project.Apps.MyApi.csproj"
+      "projectPath": "apps/my-api/Project.Apps.MyApi.csproj",
+      "files": [
+        {
+          "path": ["Directory.Build.props"], // configure update-file plugin to update fields in Directory.Build.props
+          "type": "xml",
+          "replacements": [
+            {
+              "key": "VersionPrefix",
+              "value": "${nextRelease.version}"
+            },
+            {
+              "key": "RepositoryCommit",
+              "value": "${CI_COMMIT_SHA}"
+            }
+          ]
+        }
+      ]
   };
